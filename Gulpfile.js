2afd18d12e05552d4984b9bf8ecf990efac1c8e0
Damien Senger <hi@hiwelo.co>
2015-08-08 01:52:29 +0200

    Revert "New arborescence"
    This reverts commit 1b565a910d88498e0a505cab1226470b78dda1b6.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,8 +5,8 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 // Paths
-var source = './src',
-    prod = './dist';
+var source = './assets/src',
+    prod = './assets/dist';
 
 
 // -----------------------------
@@ -15,30 +15,30 @@ var source = './src',
 
 // CSS Task = LESS & CSSO & autoprefixer
 gulp.task('css', function () {
-  return gulp.src(source + '/assets/css/styles.less')
+  return gulp.src(source + '/css/styles.less')
     .pipe(plugins.less())
     .pipe(plugins.autoprefixer())
     .pipe(plugins.csso())
     .pipe(plugins.rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest(prod + '/assets/css/'));
+    .pipe(gulp.dest(prod + '/css/'));
 });
 
 // JS Task = concatenation and copying, don't forget to add vendors
 gulp.task('js', function () {
   return gulp.src([
-    source + '/assets/js/*.js',
-    source + '/assets/vendor/jquery/dist/jquery.min.js' // jQuery vendor
+    source + '/js/*.js',
+    source + '/vendor/jquery/dist/jquery.min.js' // jQuery vendor
   ])
     .pipe(plugins.concat('global.min.js'))
     .pipe(plugins.uglify())
-    .pipe(gulp.dest(prod + '/assets/js/'));
+    .pipe(gulp.dest(prod + '/js/'));
 });
 
 // IMG Task = optimization & copying
 gulp.task('img', function () {
-  return gulp.src(source + '/assets/img/*.{png,jpg,jpeg,gif,svg}')
+  return gulp.src(source + '/img/*.{png,jpg,jpeg,gif,svg}')
     .pipe(plugins.imagemin({
       svgoPlugins: [{
         removeViewBox: false
@@ -46,13 +46,13 @@ gulp.task('img', function () {
         cleanupIDs: false
       }]
     }))
-    .pipe(gulp.dest(prod + '/assets/img'));
+    .pipe(gulp.dest(prod + '/img'));
 });
 
 // FONT Task = file copying (src -> prod)
 gulp.task('fonts', function () {
-  return gulp.src(source + '/assets/fonts/*')
-    .pipe(gulp.dest(prod + '/assets/fonts'));
+  return gulp.src(source + '/fonts/*')
+    .pipe(gulp.dest(prod + '/fonts'));
 });
 
 
@@ -62,7 +62,7 @@ gulp.task('fonts', function () {
 
 gulp.task('build', ['css', 'js', 'img', 'fonts']);
 gulp.task('watch', function () {
-  gulp.watch(source + '/assets/css/*.less', ['css']);
-  gulp.watch(source + '/assets/js/*.js', ['js']);
+  gulp.watch(source + '/css/*.less', ['css']);
+  gulp.watch(source + '/js/*.js', ['js']);
 });
 gulp.task('default', ['build']);
